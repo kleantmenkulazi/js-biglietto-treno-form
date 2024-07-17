cfc0df4397043756d121a00e9eea197fbd20ca8a
kleantmenkulazi <kleantmenkulazi@gmail.com>
2024-07-17 06:13:15 +0200

    commit intermedio

--- js/script.js
+++ js/script.js
@@ -2,38 +2,47 @@ console.log('JS collegato');
 
 
 
-const km = prompt('Quanti KM devi percorrere?');
-console.log('km', km, typeof km);
-const kmInNumber = parseInt(km);
-console.log('kmInNumber', kmInNumber, typeof kmInNumber);
 
-
-const age = prompt('Qual è la tua età ?');
-console.log('age', age, typeof age);
-const ageInNumber = parseInt(age);
-console.log('ageInNumber', ageInNumber, typeof ageInNumber);
-
-if (isNaN(ageInNumber) || isNaN(kmInNumber)) {
-    alert('Mannaggia a te');
-}
-else {
-    let price = kmInNumber * 0.21;
-    console.log('price', price, typeof price);
-
-
-
-    if (ageInNumber < 18) {
-        console.log('Minorenne');
-
-        price *=0.8;
-        console.log('price', price, typeof price);
+const calcBtn = document.getElementById('calc-btn');
+calcBtn.addEventListener(
+    'click', 
+    function () {const kmInput = document.getElementById('km');
+    const km = kmInput.value;
+    console.log('km', km, typeof km);
+    const kmInNumber = parseInt(km);
+    console.log('kmInNumber', kmInNumber, typeof kmInNumber);
+    
+    const ageInput = document.getElementById('age');
+    const age = ageInput.value;
+    console.log('age', age, typeof age);
+    const ageInNumber = parseInt(age);
+    console.log('ageInNumber', ageInNumber, typeof ageInNumber);
+    
+    if (isNaN(ageInNumber) || isNaN(kmInNumber)) {
+        alert('Mannaggia a te');
     }
-    else if (ageInNumber > 65) {
-        console.log('Over 65');
-
-        price *=0.6;
+    else {
+        let price = kmInNumber * 0.21;
         console.log('price', price, typeof price);
-    }
+    
+    
+    
+        if (ageInNumber < 18) {
+            console.log('Minorenne');
+    
+            price *=0.8;
+            console.log('price', price, typeof price);
+        }
+        else if (ageInNumber > 65) {
+            console.log('Over 65');
+    
+            price *=0.6;
+            console.log('price', price, typeof price);
+        }
+    
+        const resultSpan = document.getElementById('result');
+        resultSpan.innerHTML = price.toFixed(2);
+        }
 
-    alert('Il prezzo del biglietto è euro' + price.toFixed(2));
-}
+    }
+);
